00cd2db83e00da8feb8ed943b3a1dd21456ceb81
Jacopo Luri <jacopo.luri@capgemini.com>
2022-04-27 18:48:09 +0200

    readme

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 import imagemin from "imagemin";
 import imageminWebp from "imagemin-webp";
 
-// Setup: lancia npm i da terminale
-// 1 Metti le immagini dentro la cartella images
-// 2 Lanci node index.js da terminale
-// 3 Le immagini in webp sono nella cartella specificata (imagesWebp come default, ma puoi creare la cartella che vuoi)
-
 imagemin(["./images/*.{jpg,png}"], {
   destination: "./imagesWebp/",
   plugins: [
