af30d9761e75862dc409277cdf079d6d443fff63
Jacopo Luri <jacopo.luri@uqido.com>
2024-02-14 16:53:09 +0100

    added images quality fix

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 
 ## Setup: lancia npm i da terminale
 ## 1 Metti le immagini dentro la cartella images
-## 2 Lanci node index.js da terminale
-## 3 Le immagini in webp sono nella cartella specificata (imagesWebp come default, ma puoi creare la cartella che vuoi)
+## 2 Lancia npm run start da terminale
+## 3 Le immagini in webp sono nella cartella specificata (imagesWebp come default)
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import imagemin from "imagemin";
 import imageminWebp from "imagemin-webp";
 
-imagemin(["./images/*.{jpg,png}"], {
-  destination: "./imagesWebp/",
-  plugins: [
-    imageminWebp({}),
-  ],
-}).then(() => {
-  console.log("Images Converted Successfully!!!");
-});
+try {
+  imagemin(["./images/*.{jpg,png}"], {
+    destination: "./imagesWebp/",
+    plugins: [imageminWebp({ quality: 100, method: 6, lossless: 9 })],
+  }).then(() => {
+    console.log("Images Converted Successfully!!!");
+  });
+} catch (error) {
+  console.log(error);
+}
